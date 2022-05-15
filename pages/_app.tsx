3766fa29dead3aeefbd837cc62e4faf6fff27ea8
Suzie Kim <suuuzie18@gmail.com>
2022-05-15 22:25:40 +0900

    Add image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import 'swiper/swiper.min.css';
 import 'swiper/components/pagination/pagination.min.css';
 
 import '../styles/index.css';
-import { TITLE } from '../lib/constants';
+import { HOME_OG_IMAGE_URL, TITLE } from '../lib/constants';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -20,6 +20,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="twitter:creator" content="@iyagiclub" />
         <meta name="twitter:title" content="이야기 클럽" />
         <meta name="twitter:description" content="이야기가 펼쳐지는 곳, 이야기 클럽" />
+        <meta name="twitter:image" content={HOME_OG_IMAGE_URL} />
         <script src="https://developers.kakao.com/sdk/js/kakao.js" />
       </Head>
 
