32866ce57349dbefeef7d30f26fd2e5bc501fea4
Suzie Kim <suuuzie18@gmail.com>
2022-05-15 22:23:16 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, maximum-scale=3, minimum-scale=1, user-scalable=no, viewport-fit=cover"
         />
         <title>{TITLE}</title>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@iyagiclub" />
+        <meta name="twitter:title" content="이야기 클럽" />
+        <meta name="twitter:description" content="이야기가 펼쳐지는 곳, 이야기 클럽" />
         <script src="https://developers.kakao.com/sdk/js/kakao.js" />
       </Head>
 
