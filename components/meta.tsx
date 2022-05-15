4f29ff309909b5193b0722b443af9f485b46ef02
Suzie Kim <suuuzie18@gmail.com>
2022-05-15 22:01:43 +0900

    chore

--- components/meta.tsx
+++ components/meta.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Meta = ({description, url}: Props) => {
   return (
-    <Head>
+    <>
       <meta name="description" content={`이야기가 펼쳐지는 곳, ${TITLE}.`} />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
       <meta name="twitter:card" content="summary" />
@@ -17,7 +17,7 @@ const Meta = ({description, url}: Props) => {
       <meta name="twitter:title" content="이야기가 펼쳐지는 곳, 이야기 클럽" />
       <meta name="twitter:description" content={description ? description : ''} />
       <meta name="twitter:image" content={HOME_OG_IMAGE_URL} />
-    </Head>
+    </>
   );
 };
 
