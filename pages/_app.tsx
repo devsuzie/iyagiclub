8dd29f1fcf52d8e4e1a31d55c30c206a19209d72
Suzie Kim <suuuzie18@gmail.com>
2022-05-15 22:33:28 +0900

    Add url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,11 +16,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, maximum-scale=3, minimum-scale=1, user-scalable=no, viewport-fit=cover"
         />
         <title>{TITLE}</title>
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@iyagiclub" />
         <meta name="twitter:title" content="이야기 클럽" />
         <meta name="twitter:description" content="이야기가 펼쳐지는 곳, 이야기 클럽" />
         <meta name="twitter:image" content={HOME_OG_IMAGE_URL} />
+        <meta property="twitter:url" content="https://www.iyagi.club" />
+
         <script src="https://developers.kakao.com/sdk/js/kakao.js" />
       </Head>
 
