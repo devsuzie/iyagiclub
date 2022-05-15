c80c8a726653ff24271bca7c65145e981d77b92d
Suzie Kim <suuuzie18@gmail.com>
2022-05-15 21:16:43 +0900

    Add auto close option on progress bar

--- pages/stories/[slug].tsx
+++ pages/stories/[slug].tsx
@@ -65,7 +65,7 @@ const Post = ({ post, morePosts, preview }: Props) => {
                 <Link href="/#stories">돌아가기</Link>
               </Button>
               <Button onClick={handleShareClick}>공유하기</Button>
-              <StyledContainer hideProgressBar={true} limit={2} draggablePercent={60} />
+              <StyledContainer hideProgressBar={true} autoClose={1500} limit={2} draggablePercent={60} />
             </div>
           </>
         )}
