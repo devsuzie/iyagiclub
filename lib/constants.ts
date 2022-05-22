82f6c89b403d3cb704d82e4adc05d7f624f76735
Suzie Kim <suuuzie18@gmail.com>
2022-05-22 19:36:30 +0900

    Update image url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
+import { BASE_PATH } from "../constants/idex";
+
 export const HOME_TITLE = '이야기클럽';
 export const HOME_URL = 'https://www.iyagi.club/'
-export const HOME_OG_IMAGE_URL = '/assets/blog/preview/cover.jpg';
+export const HOME_OG_IMAGE_URL = `${BASE_PATH}/assets/blog/preview/cover.jpg`;
